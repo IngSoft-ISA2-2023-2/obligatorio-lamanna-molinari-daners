9156ac498232da71014602db3eff0bf69c4cec5d
Melissa <meli-giva@hotmail.com>
2023-10-20 21:03:41 -0300

    create product- IProductManager creation

---
+++ Código/Backend/PharmaGo.IBusinessLogic/IProductManager.cs
@@ -0,0 +1,19 @@
+﻿using ExportationModel.ExportDomain;
+using PharmaGo.Domain.Entities;
+using PharmaGo.Domain.SearchCriterias;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmaGo.IBusinessLogic
+{
+    public interface IProductManager
+    {
+        Product GetById(int id);
+        Product Create(Product product, string token);
+        Product Update(int id, Product drug);
+        void Delete(int id);
+    }
+}
